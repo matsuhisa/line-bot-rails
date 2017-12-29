05516e8807707bb578f6959b4ea2a01cb3163e73
matsuhisa <matsuhisa+github@gmail.com>
2017-12-29 23:39:47 +0900

    Add def callback for line api

--- app/controllers/api/line_controller.rb
+++ app/controllers/api/line_controller.rb
@@ -1,4 +1,40 @@
 class Api::LineController < ApplicationController
+  protect_from_forgery with: :null_session
+  require 'line/bot'
+
   def callback
+    body = request.body.read
+
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    unless client.validate_signature(body, signature)
+      error 400 do 'Bad Request' end
+    end
+
+    events = client.parse_events_from(body)
+    events.each do |event|
+      case event
+      when Line::Bot::Event::Postback
+        client.reply_message(event['replyToken'], event['postback']['data'])
+      when Line::Bot::Event::Message
+        case event.type
+        when Line::Bot::Event::MessageType::Text
+          client.reply_message(event['replyToken'], message)
+        when Line::Bot::Event::MessageType::Image, Line::Bot::Event::MessageType::Video
+          response = client.get_message_content(event.message['id'])
+          tf = Tempfile.open("content")
+          tf.write(response.body)
+        end
+      end
+    end
+    render :nothing => true, status: :ok
+  end
+
+  private
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    }
   end
 end
