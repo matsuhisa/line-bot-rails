e5ade766b936118af573fcac11458b765ba579cd
matsuhisa <matsuhisa+github@gmail.com>
2017-12-30 12:13:04 +0900

    Add json

--- app/controllers/api/line_controller.rb
+++ app/controllers/api/line_controller.rb
@@ -7,7 +7,9 @@ class Api::LineController < ApplicationController
 
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      error 400 do 'Bad Request' end
+      head :bad_request
+      # status: 400
+      # error 400 do 'Bad Request' end
     end
 
     events = client.parse_events_from(body)
@@ -16,10 +18,65 @@ class Api::LineController < ApplicationController
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
+          # message = {
+          #   type: 'text',
+          #   text: event.message['text']
+          # }
+
           message = {
-            type: 'text',
-            text: event.message['text']
+            "type": "template",
+            "altText": "this is a carousel template",
+            "template": {
+                "type": "carousel",
+                "columns": [
+                    {
+                      "thumbnailImageUrl": "https://farm5.staticflickr.com/4244/35370997765_5f65dd60da_k_d.jpg",
+                      "title": "this is menu",
+                      "text": "description",
+                      "actions": [
+                        {
+                          type: "postback",
+                          label: "Buy",
+                          data: "action=buy&itemid=123"
+                        },
+                        {
+                          type: "uri",
+                          label: "電話する",
+                          uri: "tel:09012345678"
+                        },
+                        {
+                          type: "uri",
+                          label: "詳細",
+                          uri: "https://www.flickr.com/photos/matsuhisa/35370997765"
+                        }
+                      ]
+                    },
+                    {
+                      "thumbnailImageUrl": "https://farm5.staticflickr.com/4288/35241101511_d919ed5d75_k_d.jpg",
+                      "title": "this is menu",
+                      "text": "description",
+                      "actions": [
+                        {
+                          type: "postback",
+                          label: "Buy",
+                          data: "action=buy&itemid=123"
+                        },
+                        {
+                          type: "uri",
+                          label: "電話する",
+                          uri: "tel:09012345678"
+                        },
+                        {
+                          type: "uri",
+                          label: "詳細",
+                          uri: "https://www.flickr.com/photos/matsuhisa/35241101511"
+                        }
+                      ]
+                    }
+                ]
+            }
           }
+
           response = client.reply_message(event['replyToken'], message)
           p response
         end
